94a7f8221ee9569fc561a1379c95c51b4c095a31
Sergey <immortalizedinhearts@gmail.com>
2019-09-12 21:32:12 +0300

    First model(Topic) was created and activated; learning_logs migration was successfully created and performed

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    """Theme, which user is learning"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returns string model view"""
+        return self.text
