c450cdbf07e0499be53d2dd38f08c6b7d20a1ab1
Sergey <immortalizedinhearts@gmail.com>
2019-09-12 20:39:37 +0300

    Django was added

---
+++ ll_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\acer\documents\projects\learninglog\ll_venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
